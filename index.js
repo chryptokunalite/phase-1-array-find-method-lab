d9a7d7548e2344f8384d0259894fb534f0ce8b0a
Allan Saenz <allan.d.saenz@gmail.com>
2023-06-07 20:29:35 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function firstWin(object){
+    return object.result === "W"
+}
+
+
+
+
+function superbowlWin(record){
+    let result = record.find(firstWin)
+  return result ? result.year : undefined
+}
+
